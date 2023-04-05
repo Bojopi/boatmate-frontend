bbf47eb64b9e3d2123ecf65e4170a30de2b677e3
Bryana Ojopi <ojopibryana5@gmail.com>
2023-04-05 01:47:29 -0400

    listo para modificar el panel administrativo

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -15,7 +15,7 @@ const Welcome = () => {
             img="https://i.postimg.cc/mgt5kh39/angelina-herbert-T09mdb-YH8-Ac-unsplash.jpg"
         />
         <div className="w-full p-10 md:pl-28 text-white" style={{'backgroundColor': '#00CBA4'}}>
-            <h1 className="text-xl w-full md:text-4xl font-bold flex md:w-2/3 tracking-wide">CATEGORIES</h1>
+            <h1 className="text-xl w-full md:text-4xl font-bold flex md:w-2/3 tracking-wide">Adminstrative Panel</h1>
             <p className="w-full pt-5 md:w-2/3 tracking-wide">At BoatMate we offer services including, but not limited to: technical services,  maintenance & repair services, cleaning services, transportation services, and many more.</p>
         </div>
         <CarouselComponent />
