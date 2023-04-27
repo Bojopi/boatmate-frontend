810c639eb90441a46620454c507090e4fbb9f252
Bryana Ojopi <ojopibryana5@gmail.com>
2023-04-26 20:24:33 -0400

    rewrites next config

--- next.config.js
+++ next.config.js
@@ -17,19 +17,14 @@ const nextConfig = {
       }
     ]
   },
-  async headers() {
+  async rewrites() {
     return [
       {
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
+        source: '/:path*',
+        destination: 'https://boatmate-backend-production.up.railway.app/:path*',
+      },
     ]
-  }
+  },
 }
 
 module.exports = nextConfig
