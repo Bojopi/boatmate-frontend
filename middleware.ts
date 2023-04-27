7e3b31adec4cd16cf709dd5c2be0a6374db83619
Bryana Ojopi <ojopibryana5@gmail.com>
2023-04-27 17:08:10 -0400

    probando una nueva solución 2

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,8 @@ export async function middleware(request: NextRequest) {
 
     let token = request.cookies.get("token")?.value;
 
+    console.log(token)
+
     if(!token) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
