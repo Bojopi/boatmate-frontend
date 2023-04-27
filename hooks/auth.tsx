f4cebd531ed8ec14cda8906a52a5ba986db1899f
Bryana Ojopi <ojopibryana5@gmail.com>
2023-04-26 21:46:36 -0400

    ver si trae el header

--- hooks/auth.tsx
+++ hooks/auth.tsx
@@ -20,6 +20,7 @@ export const Auth = () => {
             .post('/auth', data)
             .then((res) => {
                 // console.log('logueado')
+                console.log(res.headers['set-cookie'])
                 setLoading(false);
                 toast.current.show({severity:'success', summary:'Successful', detail: `${res.data.msg}`, life: 4000});
                 router.push('/welcome')
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,10 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { jwtVerify } from 'jose';
-import Cookies from 'js-cookie';
 
 const secret: string = process.env.SECRET!;
 
 export async function middleware(request: NextRequest) {
-    console.log(request.headers)
 
     let token = request.cookies.get("tokenUser")?.value;
 
