c63a5df67db686f518d47d68074b2b2c84ae513f
Bryana Ojopi <ojopibryana5@gmail.com>
2023-05-08 13:40:13 -0400

    hacer reconocer la variable de entorno

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { jwtVerify } from 'jose';
 
-const secret = process.env.NEXT_PUBLIC_JWT_SECRET as string;
+const secret = process.env.NEXT_PUBLIC_JWT_SECRET ? process.env.NEXT_PUBLIC_JWT_SECRET :'a1b2c3d4e5f6g7@';
 
 export async function middleware(request: NextRequest) {
 
