4ed895ad760946d56d148664dbf6510c94300534
Bryana Ojopi <ojopibryana5@gmail.com>
2023-05-08 10:50:59 -0400

    se cambió al url del axios

--- config/axios.ts
+++ config/axios.ts
@@ -1,9 +1,10 @@
 import Axios from 'axios'
 
 export const axios = Axios.create({
-    baseURL: 'http://localhost:8080/api',
+    // baseURL: 'http://localhost:8080/api',
     // baseURL: 'https://boatmate-backend-production.up.railway.app/api',
     // baseURL: 'http://ec2-3-131-141-161.us-east-2.compute.amazonaws.com:8080/api',
+    baseURL: 'http://v2.boatmate.com:8080/api',
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
